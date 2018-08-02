aa8ec1850e77b8b2761f02663fa683acd347b6a1
meshari1077 <meshari@adebas.net>
2018-08-02 04:58:05 +0300

    hh
    hh

--- header1.php
+++ header1.php
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 
 <div class="header">
 <div class="row ">
@@ -16,3 +17,28 @@
 </div>
 </div>
 </div>
+=======
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Portfolio</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+>>>>>>> b6c9fbbc6faad5e18e0ef7d765f0e31354a32042
