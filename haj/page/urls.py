d01899322d135b928f136f937c62e7784a18d7f7
moha <moha.alshawi@hotmail.com>
2018-08-02 06:08:50 +0300

    Delate Everything

--- haj/page/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path , include
-from . import views
-
-urlpatterns = [
-    path('' , views.MainPage),
-    path('MedicalServices/' , views.MedicalServicesView),
-    path('Lost/' , views.LostViews),
-]
--- page/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.MedicalServices)
-admin.site.register(models.Lost) 
-# Register your models here.
